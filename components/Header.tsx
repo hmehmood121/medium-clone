b473b5604e3536eb200338876f1e40a77bf45c56
Hasan Mehmood <hmehmood121@gmail.com>
2023-03-30 23:36:57 +0500

    logo added

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,8 @@ function Header() {
     <header className="flex justify-between p-5 max-w-7xl mx-auto">
         <div className="flex items-center space-x-5">
             <Link href="/">
-                <img className="w-44 object-contain cursor-pointer" src="https://links.papareact.com/yvf" alt="" />
+                {/* <img className="w-44 object-contain cursor-pointer" src="https://links.papareact.com/yvf" alt="" /> */}
+                <h1 className="font-bold">Agriinova</h1>
             </Link>
             <div className="hidden md:inline-flex items-center space-x-3">
                 <h3>About</h3>
